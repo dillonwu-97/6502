023ae97298eadef137f81802309a677c9e6843af
Dillon Wu <dillonw@andrew.cmu.edu>
2024-12-01 18:30:39 -0500

    updated the emulator to actually be useful

--- 6502/mos/src/main.rs
+++ 6502/mos/src/main.rs
@@ -1,57 +1,113 @@
-use std::fs::File;
-use std::path::Path;
-use std::fs;
+mod opcodes;
+use opcodes::Opcode;
+use bitflags::bitflags;
 
-// Simple test program to open file, and split the file as bytes into an array of 4 byte chunks 
+const MEMSIZE: usize = 2 << 16;
 
-// interesting, the stack underflows 
-//
+bitflags! {
+    struct  StatusRegister: u8 {
+        const C = 0b0000_0001; // carry
+        const Z = 0b0000_0010; // zero
+        const I = 0b0000_0100; // interrupt
+        const D = 0b0000_1000; // decimal 
+        const B = 0b0001_0000; // break
+                    // unused 5th bit
+        const V = 0b0100_0000; // overflow
+        const N = 0b1000_0000; // negative
+    }
+}
 
-//N	Negative
-//V	Overflow
-//-	ignored
-//B	Break
-//D	Decimal (use BCD for arithmetics)
-//I	Interrupt (IRQ disable)
-//Z	Zero
-//C	Carry
-fn emulate() {
-    let pc:u16 = 0;
-    let ac:u8 = 0;
-    let x:u8 = 0;
-    let y:u8 = 0;
-    let sr:u8 = 0;
-    let sp:u8 = 0;
+struct CPU {
+
+    memory: [u8; MEMSIZE],
+    pc: u16,
+    ac: u8, // accumulator
+    x: u8,
+    y: u8,
+    sr: StatusRegister, // bitfield for the status bits
+    // maybe define some constants and then do the | operation? 
+    // not really sure lol 
+    sp: u8,
+
+    // TODO: What should the size of this be?
+    // TODO: Is each opcode equal to a cycle? I believe so 
+    // I feel like I don't really need to keep track of this until later
+    //cycles: u32,
+}
+
+impl CPU {
+    pub fn new() -> Self {
+        Self {
+            pc: 0, ac: 0, x: 0, y: 0, sr: StatusRegister::empty(), sp: 0xff   , 
+            memory: [0; MEMSIZE],
+        }
+    }
+    
+    pub fn reset(&mut self) {
+        // Reset the CPU
+        self.pc = 0;
+        self.ac = 0;
+        self.x = 0;
+        self.y = 0;
+        self.sr = StatusRegister::empty();
+        self.sp = 0xff;
+        self.memory = [0; MEMSIZE];
+    }
+
+    // TODO: should these implementations be somewhere else?
+    fn set_status(&mut self, flag: StatusRegister) {
+        self.sr.insert(flag);
+    }
+
+    fn clear_status(&mut self, flag: StatusRegister) {
+        self.sr.remove(flag);
+    }
+
+    fn get_status(&mut self, flag: StatusRegister) -> bool {
+        self.sr.contains(flag)
+    }
+
+    // grab the bytecode?
+    // no need to bee immutable i think 
+    fn fetch_byte(&mut self) -> u8 {
+        let byte = self.memory[self.pc as usize];
+        self.pc += 1;
+        byte 
+    }
+
+    pub fn execute(&mut self) {
+        // Execute an instruction
+        let opcode = self.fetch_byte();
+        match opcode {
+            LDA => { // load immediate opcode
+                let value = self.fetch_byte();
+                self.ac = value;
+                if self.ac == 0 {
+                    self.set_status(StatusRegister::Z);
+                }
+                if self.get_status(StatusRegister::V) {
+                    self.set_status(StatusRegister::N);
+                }
+            }
+            _   => println!("Instruction not found")
+        }
+    }
+
 
 }
+
+
 
 fn main() {
-    let path = Path::new("./data/pacman");
-    let display = path.display();
-
-    let mut file = match File::open(&path) {
-        Err(why) => panic!("Couldn't open file {} because {}", display, why),
-        Ok(file) => file,
-    };
-
-    println!("Hello, world!");
-    println!("{}", display);
-
-    let data: Vec<u8> = match fs::read("./data/pacman") {
-        Err(why) => panic!("data fail"),
-        Ok(data) => data,
-    };
-
-    // naive method is to separate into 2,3,4 partitions 
-    // then match with each of them 
-    // do a recursive traversal algorithm?
-    for (i, &item) in data.iter().enumerate() {
-
-        println!("{}", item);
-
-    }
-    println!("print is done!");
-
+    println!("hello world");
+    let mut cpu = CPU::new();
+    cpu.reset();
+    cpu.memory[0x0] = 0xA9;
+    cpu.memory[0x1] = 0x41;
+    cpu.execute();
+    println!("Accumulator: {}", cpu.ac);
 }
+
+
 
 
---
+++ 6502/mos/src/opcodes.rs
@@ -0,0 +1,4 @@
+pub enum Opcode {
+    LDA = 0xA9,  // LDA #
+    LDA_ZPG = 0xA5,
+}
