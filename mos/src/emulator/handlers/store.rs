7ce69b0af48b03b420806cfe82fba4172797b522
dillon wu <dillonwu999@gmail.com>
2025-01-31 16:13:17 -0800

    store opcodes

--- mos/src/emulator/handlers/store.rs
+++ mos/src/emulator/handlers/store.rs
@@ -1,82 +1,22 @@
 #![allow(non_snake_case)]
 use crate::emulator::CPU;
-use crate::emulator::cpu::StatusRegister;
-use crate::emulator::Opcode;
+use crate::emulator::Inst;
 
 impl CPU {
-    
-    // Returns found_opcode or not
-    pub(crate) fn st_exec(&mut self, opcode: u8) -> bool {
-        return self.st_acc(opcode) || self.st_x(opcode) || self.st_y(opcode);
-    }
 
-    pub(crate) fn st_acc(&mut self, opcode: u8) -> bool {
-        let op = Opcode::from(opcode);
-        match op {
-            Opcode::STA_ZPG => {
-                self.memory[ self.fetch_byte() as usize ] = self.ac;
+    pub fn st(&mut self, inst: Inst) -> u8 {
+        match inst {
+            Inst::STA => {
+                return self.ac;
             }
-            Opcode::STA_ZPX => {
-                let addr = self.fetch_byte() + self.x;
-                self.memory[ addr as usize ] = self.ac;
+            Inst::STX => {
+                return self.x;
             }
-            Opcode::STA_ABS => {
-                self.memory[ self.fetch_two() as usize ] = self.ac;
+            Inst::STY => {
+                return self.y;
             }
-            Opcode::STA_ABX => {
-                let addr = self.fetch_two() + (self.x as u16);
-                self.memory[ addr as usize ] = self.ac;
-            }
-            Opcode::STA_ABY => {
-                let addr = self.fetch_two() + (self.y as u16);
-                self.memory[ addr as usize ] = self.ac;
-            }
-            Opcode::STA_INX => {
-            }
-            Opcode::STA_INY => {
-            }
-            _ => {
-                return false; 
-            }
+            _ => panic!("Should never reach this case"),
         }
-        true
-    }
 
-    pub(crate) fn st_x(&mut self, opcode: u8) -> bool {
-        let op = Opcode::from(opcode);
-        match op {
-            Opcode::STX_ZPG => {
-                self.memory[ self.fetch_byte() as usize ] = self.x;
-            }
-            Opcode::STX_ZPY => {
-                self.memory[ (self.fetch_byte() + self.y) as usize ] = self.x;
-            }
-            Opcode::STX_ABS => {
-                self.memory[ self.fetch_two() as usize ] = self.x;
-            }
-            _ => {
-                return false; 
-            }
-        }
-        true
-    }
-
-    pub(crate) fn st_y(&mut self, opcode: u8) -> bool {
-        let op = Opcode::from(opcode);
-        match op {
-            Opcode::STY_ZPG => {
-                self.memory[ self.fetch_byte() as usize ] = self.y;
-            }
-            Opcode::STY_ZPX => {
-                self.memory[ (self.fetch_byte() + self.x) as usize ] = self.y;
-            }
-            Opcode::STY_ABS => {
-                self.memory[ self.fetch_two() as usize ] = self.y;
-            }
-            _ => {
-                return false;
-            }
-        }
-        true
     }
 }
