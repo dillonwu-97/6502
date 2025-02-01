071bcf93332702333bfa15b86d3b5d3bbed33d49
dillon wu <dillonwu999@gmail.com>
2025-01-31 16:13:29 -0800

    transfer opcodes

--- mos/src/emulator/handlers/transfer.rs
+++ mos/src/emulator/handlers/transfer.rs
@@ -3,12 +3,12 @@
 #![allow(non_snake_case)]
 use crate::emulator::CPU;
 use crate::emulator::cpu::StatusRegister;
-use crate::emulator::Opcode;
+use crate::emulator::Inst;
 
 
 impl CPU {
 
-    pub(crate) fn tx_set_status(&mut self, register: u8) {
+    pub fn tx_set_status(&mut self, register: u8) {
         if register == 0 {
             self.set_status(StatusRegister::Z);
         }
@@ -17,36 +17,25 @@ impl CPU {
         }
     }
 
-    pub(crate) fn tx_exec(&mut self, opcode: u8) -> bool {
-        let op = Opcode::from(opcode);
-        match op {
-            Opcode::TAX => {
+    pub fn tx(&mut self, inst: Inst) {
+        match inst {
+            Inst::TAX => {
                 self.x = self.ac;
                 self.tx_set_status(self.x);
             }
-            Opcode::TAY => {
+            Inst::TAY => {
                 self.y = self.ac;
                 self.tx_set_status(self.y);
             }
-            Opcode::TSX => {
-                self.x = self.sp;
-                self.tx_set_status(self.x);
-            }
-            Opcode::TXA => {
+            Inst::TXA => {
                 self.ac = self.x;
                 self.tx_set_status(self.ac);
             }
-            Opcode::TXS => {
-                self.sp = self.x;
-            }
-            Opcode::TYA => {
+            Inst::TYA => {
                 self.ac = self.y;
                 self.tx_set_status(self.ac);
             }
-            _ => {
-                return false;
-            }
-        }
-        true
-    }
+            _ => panic!("Should not reach this instruction")
+        } 
+    } 
 }
