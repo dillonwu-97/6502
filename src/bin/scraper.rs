d632081c0bf2308f37d75b754e4de1f923c7d052
dillon <dillonwu999@gmail.com>
2025-12-04 19:21:45 +0000

    scraper added for nop codes and jam codes

--- src/bin/scraper.rs
+++ src/bin/scraper.rs
@@ -1,17 +1,19 @@
+use mos::emulator::opcodes;
 use serde::Serialize;
 use std::collections::HashMap;
-use std::fs::File;
-use std::io::prelude::*;
+use std::fmt::format;
+use std::fs::{read, File};
 use std::io::{self, BufRead};
+use std::io::{prelude::*, Lines};
 use std::path::Path;
 
 // TODO: move this to a different file and allow it to be imported / reused elsewhere in the code
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, Clone)]
 struct Opcode {
     inst: String,
     mode: String,
     cycle_count: u8,
-    opcode: u8,
+    opcode: String,
     pagex: bool, // checks if the instruction crosses page boundary and incurs an extra cycle count
 }
 
@@ -72,7 +74,7 @@ fn construct_opcode_vec(
     let mut opcode_vec: Vec<Opcode> = Vec::new();
     let mut inst: String;
     let mut mode: String;
-    let mut opcode: u8;
+    let mut opcode: String;
     let mut cycle_count: u8;
     let mut pagex: bool;
 
@@ -98,7 +100,9 @@ fn construct_opcode_vec(
             pagex = false;
             cycle_count = s_cycle_cnt.parse::<u8>().unwrap();
         }
-        opcode = u8::from_str_radix(tokens[tokens.len() - 3].trim(), 16).unwrap();
+        // opcode = u8::from_str_radix(tokens[tokens.len() - 3].trim(), 16).unwrap();
+        // opcode = u8::from_str_radix(tokens[tokens.len() - 3].trim(), 16)
+        opcode = String::from(tokens[tokens.len() - 3].trim());
 
         // println!("mode 2: {} {} {}", mode, inst, opcode);
 
@@ -115,6 +119,99 @@ fn construct_opcode_vec(
     return opcode_vec;
 }
 
+fn add_nop_codes<P>(path: P, addr_modes: &HashMap<&str, &str>) -> Vec<Opcode>
+where
+    P: AsRef<Path>,
+{
+    // NOP 1A	implied	1	2
+    // NOP 3A	implied	1	2
+    // NOP 5A	implied	1	2
+
+    let mut lines_to_iter: Vec<String> = Vec::new();
+    match read_lines(path) {
+        Ok(lines) => {
+            lines
+                .filter(|f| {
+                    if let Ok(line) = f {
+                        line.split_whitespace().next() == Some("NOP")
+                    } else {
+                        false
+                    }
+                })
+                .for_each(|v| lines_to_iter.push(v.unwrap()));
+        }
+        Err(_) => {
+            println!("bad")
+        }
+    }
+    let mut nop_vec: Vec<Opcode> = Vec::new();
+    // println!("add nop codes");
+    let mut inst: String;
+    let mut mode: String;
+    let mut cycle_count: u8;
+    let mut opcode: String;
+    let mut pagex: bool;
+
+    for line in lines_to_iter {
+        let tokens: Vec<&str> = line.split_whitespace().collect();
+        inst = tokens[0].to_string();
+        opcode = tokens[1].to_string();
+        mode = addr_modes.get(tokens[2]).unwrap().to_string();
+        if tokens[tokens.len() - 1].contains("*") {
+            pagex = true;
+            cycle_count = tokens[tokens.len() - 1]
+                .replace("*", "")
+                .parse::<u8>()
+                .unwrap();
+        } else {
+            pagex = false;
+            cycle_count = tokens[tokens.len() - 1].parse::<u8>().unwrap();
+        }
+        nop_vec.push(Opcode {
+            inst: inst,
+            mode: mode,
+            opcode: opcode,
+            cycle_count: cycle_count,
+            pagex: pagex,
+        });
+        println!("nop {}", line);
+    }
+
+    // for op in nop_vec.clone() {
+    //     println!("{:?}", op);
+    // }
+    nop_vec
+}
+/*
+ * In the document, the nop codes are formatted differently, so we have another function to add
+ * just those
+ */
+
+fn add_jam_codes() -> Vec<Opcode> {
+    /*
+     * In the document, the JAM opcodes are formatted differently, so we have another fn to handle
+     * those
+     */
+    let jam: Vec<u8> = vec![
+        0x02, 0x12, 0x22, 0x32, 0x42, 0x52, 0x62, 0x72, 0x92, 0xB2, 0xD2, 0xF2,
+    ];
+    let s_jam = jam.iter().map(|f| format!("{:02X}", f));
+
+    let mut jam_op: Vec<Opcode> = Vec::new();
+    for op in s_jam {
+        jam_op.push(Opcode {
+            inst: String::from("JAM"),
+            mode: String::from("NA"),
+            opcode: op,
+            cycle_count: 0,
+            pagex: false,
+        })
+    }
+    jam_op
+}
+
+// TODO:modify the code so that we open the file only once and grab the data without constantly
+// reopening
 fn main() {
     let addr_modes = HashMap::from([
         ("implied", "IMP"),
@@ -152,25 +249,31 @@ fn main() {
     let illegal_opcode_lines: Vec<String> = reformat_lines(ill_path, &addr_modes);
 
     let mut opcode_vec: Vec<Opcode> = Vec::new();
+
+    // TODO: need a clean cp of the opened file
+    let mut nop_lines = lines_to_parse.clone();
     opcode_vec = construct_opcode_vec(lines_to_parse, &addr_modes);
-    println!("Done");
+
+    // TODO: use of moved value
 
     let mut opcode_vec_2: Vec<Opcode> = Vec::new();
     println!("{}", illegal_opcode_lines.len());
     opcode_vec_2 = construct_opcode_vec(illegal_opcode_lines, &addr_modes);
-    // opcode_vec_2
-    //     .iter()
-    //     .for_each(|f| println!("hi {}", f.opcode));
 
+    let mut nop_vec: Vec<Opcode> = add_nop_codes(ill_path, &addr_modes);
+    let mut jam_vec: Vec<Opcode> = add_jam_codes();
     opcode_vec.extend(opcode_vec_2);
+    opcode_vec.extend(nop_vec);
+    opcode_vec.extend(jam_vec);
 
+    // opcode_vec_2
+    //     .clone()
+    //     .iter()
+    //     .for_each(|f| println!("{}", f.opcode));
+    opcode_vec.sort_by(|a, b| a.opcode.cmp(&b.opcode));
+    println!("Length: {}", opcode_vec.len());
     let json_str: String = String::from("./temp.json");
     dump_to_json(json_str, &opcode_vec);
 }
 
-// TODO: need special case to handle all the halt instructions as well
-//
-//
-//
-//
-//
+// TODO: need special case to handle all the NOP and halt instructions as well
