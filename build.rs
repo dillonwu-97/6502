cc9eb3fd9cb902d395bd4e98f39ea926a589f6f1
dillon <dillonwu999@gmail.com>
2026-08-07 22:43:27 -0400

    metaprogramming for opcodes in build file

--- build.rs
+++ build.rs
@@ -1,31 +1,42 @@
-use std::env;
-use std::fs;
-use std::path::Path;
-use std::fs::File;
-use std::io::{BufReader,Read};
 use serde_json::Value;
 use std::collections::BTreeSet;
+use std::fs::File;
+use std::io::BufReader;
 
-fn build_enum(src: BTreeSet<String>, name: String) -> String {
+fn build_enum<I>(src: I, name: String) -> String
+where
+    I: IntoIterator<Item = String>,
+{
     let mut ret = format!("#[derive(Debug, PartialEq, Eq)]\npub enum {} {{\n", name);
 
-    for (i, v) in src.iter().enumerate() {
+    for (i, v) in src.into_iter().enumerate() {
         ret.push_str(&format!("   {} = 0x{:02x}\n", v, i));
     }
 
     ret.push_str("}\n");
     println!("{}", ret);
-    ret 
+    ret
 }
 
+fn get_iters<C: FromIterator<String>>(ops: Value, fields: Vec<String>) -> C {
+    ops.as_array()
+        .unwrap()
+        .into_iter()
+        .filter_map(|v| v.get("inst")?.as_str())
+        .map(|s| s.to_string())
+        .collect()
+}
 
 fn main() {
     // Tell cargo to rerun this build script if opcodes.txt changes
     //
-    let file = File::open("./opcodes.json").unwrap();
+    let file = File::open("./assets/opcodes.json").unwrap();
     let reader = BufReader::new(file);
-    let ops:Value = serde_json::from_reader(reader).unwrap();
-    let instructions: BTreeSet<String> = ops.as_array().unwrap().into_iter()
+    let ops: Value = serde_json::from_reader(reader).unwrap();
+    let instructions: BTreeSet<String> = ops
+        .as_array()
+        .unwrap()
+        .into_iter()
         .filter_map(|v| v.get("inst")?.as_str())
         .map(|s| s.to_string())
         .collect();
@@ -34,19 +45,31 @@ fn main() {
     //     println!("{}, {:?}",i, v);
     // }
 
-    let addr_modes: BTreeSet<String> = ops.as_array().unwrap().into_iter()
+    let addr_modes: BTreeSet<String> = ops
+        .as_array()
+        .unwrap()
+        .into_iter()
         .filter_map(|v| v.get("mode")?.as_str())
         .map(|s| s.to_string())
         .collect();
 
-    let opcodes: BTreeSet<String> = ops.as_array().unwrap().into_iter()
+    let opcodes: Vec<String> = ops
+        .as_array()
+        .unwrap()
+        .into_iter()
         .filter_map(|v| {
-            !format("{}_{}",v.get("inst")?.as_str(),v.get("mode")?.as_str())
+            Some(format!(
+                "{}_{}",
+                v.get("inst")?.as_str()?,
+                v.get("mode")?.as_str()?
+            ))
         })
         .collect();
 
+    println!("{:?}", opcodes);
     _ = build_enum(addr_modes, "AddrMode".to_string());
     _ = build_enum(instructions, "Inst".to_string());
+    _ = build_enum(opcodes, "Opcode".to_string());
 
     // println!("{}", addr_modes.len());
     // for (i,v) in addr_modes.into_iter().enumerate() {
@@ -55,9 +78,8 @@ fn main() {
 
     // build_addr_mode();
 
-
     // let mut addr_modes = String::from("#[derive(Debug, PartialEq, Eq)]\npub enum AddrMode {\n");
-    
+
     // for line in addr_modes.lines() {
     //     let name = line.trim();
     //     if !name.is_empty() {
