618fc509e488cee90bf2a68cc44cece18dc8c688
dillon <dwu@cyber.nyc.gov>
2026-08-14 18:25:24 -0400

    build file updated

--- build.rs
+++ build.rs
@@ -3,6 +3,24 @@ use std::collections::BTreeSet;
 use std::fs::File;
 use std::io::BufReader;
 
+// Build the string to use 
+fn get_iters<C: FromIterator<String>>(ops: Value, fields: Vec<String>) -> C {
+    ops.as_array()
+        .unwrap()
+        .into_iter()
+        .filter_map(|v| {
+            let mut ret: String = String::new();
+            for (i,j) in fields.iter().enumerate() {
+                ret.push_str(v.get(j)?.as_str()?);
+                if i != fields.len()-1 {
+                    ret.push_str("_");
+                }
+            }
+            Some(ret)
+        })
+        .collect()
+}
+
 fn build_enum<I>(src: I, name: String) -> String
 where
     I: IntoIterator<Item = String>,
@@ -18,75 +36,56 @@ where
     ret
 }
 
-fn get_iters<C: FromIterator<String>>(ops: Value, fields: Vec<String>) -> C {
-    ops.as_array()
-        .unwrap()
-        .into_iter()
-        .filter_map(|v| v.get("inst")?.as_str())
-        .map(|s| s.to_string())
-        .collect()
+// TODO: fix the way this looks build for arbitary i think 
+fn build_impl<I> (src: I, name: String) -> String 
+where
+    I: IntoIterator<Item = String>
+{
+    let mut ret: String = format!("impl From <u8> for {} {{\n\t\
+    fn from(value: u8) -> Self {{\n\t\t\
+    match value {{\n\t\t\t\
+    ", name).to_string();
+    for (i,v) in src.into_iter().enumerate() {
+        ret.push_str(&format!("\t 0x{:02x} => {}::{}\n", i, name, v));
+    }
+    ret.push_str("\t\t}\n\t}\n}");
+    ret
+}
+
+fn build_struct() -> String {
+    "#[derive(Clone)]
+    pub struct OpWrapper {
+        pub op: Opcode,
+        pub inst: Inst,
+        pub addr_mode: AddrMode,
+        pub cycle: u8,
+        pub pagex: bool,
+    }".to_string()
+
+  // {
+  //   "inst": "SLO",
+  //   "mode": "ABY",
+  //   "cycle_count": 7,
+  //   "opcode": "1B",
+  //   "pagex": false
+  // },
 }
 
 fn main() {
     // Tell cargo to rerun this build script if opcodes.txt changes
-    //
-    let file = File::open("./assets/opcodes.json").unwrap();
+    // Put into fn 
+    let file = File::open("./opcodes.json").unwrap();
     let reader = BufReader::new(file);
     let ops: Value = serde_json::from_reader(reader).unwrap();
-    let instructions: BTreeSet<String> = ops
-        .as_array()
-        .unwrap()
-        .into_iter()
-        .filter_map(|v| v.get("inst")?.as_str())
-        .map(|s| s.to_string())
-        .collect();
-    //
-    // for (i,v) in instructions.into_iter().enumerate() {
-    //     println!("{}, {:?}",i, v);
-    // }
 
-    let addr_modes: BTreeSet<String> = ops
-        .as_array()
-        .unwrap()
-        .into_iter()
-        .filter_map(|v| v.get("mode")?.as_str())
-        .map(|s| s.to_string())
-        .collect();
-
-    let opcodes: Vec<String> = ops
-        .as_array()
-        .unwrap()
-        .into_iter()
-        .filter_map(|v| {
-            Some(format!(
-                "{}_{}",
-                v.get("inst")?.as_str()?,
-                v.get("mode")?.as_str()?
-            ))
-        })
-        .collect();
-
-    println!("{:?}", opcodes);
-    _ = build_enum(addr_modes, "AddrMode".to_string());
-    _ = build_enum(instructions, "Inst".to_string());
-    _ = build_enum(opcodes, "Opcode".to_string());
-
-    // println!("{}", addr_modes.len());
-    // for (i,v) in addr_modes.into_iter().enumerate() {
-    //     println!("{}, {:?}",i, v);
-    // }
-
-    // build_addr_mode();
-
-    // let mut addr_modes = String::from("#[derive(Debug, PartialEq, Eq)]\npub enum AddrMode {\n");
-
-    // for line in addr_modes.lines() {
-    //     let name = line.trim();
-    //     if !name.is_empty() {
-    //         enum_code.push_str(&format!("    {},\n", name));
-    //     }
-    // }
-    // enum_code.push_str("}\n");
-
-    // fs::write(&dest_path, enum_code).unwrap();
+    let addr_modes: BTreeSet<String> = get_iters(ops.clone(), vec!["mode".to_string()]);
+    let instructions: BTreeSet<String> = get_iters(ops.clone(), vec!["inst".to_string()]);
+    let opcodes: Vec<String> = get_iters(ops.clone(), vec!["inst".to_string(), "mode".to_string()]);
+    _ = build_enum(addr_modes.clone(), "AddrMode".to_string());
+    _ = build_enum(instructions.clone(), "Inst".to_string());
+    // _ = build_enum(opcodes.clone(), "Opcode".to_string());
+    let a = build_impl(addr_modes, "AddrMode".to_string());
+    let b = build_impl(instructions, "Inst".to_string());
+    let c = build_impl(opcodes, "Opcode".to_string());
+    println!("{}",b);
 }
